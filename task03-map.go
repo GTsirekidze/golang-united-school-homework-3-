3e18cfa40f75751b07fe78f0529147e59d149ab4
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-05-07 02:33:24 +0400

    ""

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,19 @@
 package homework
 
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	mn := -9999999
+	for range input {
+		mn2 := -99999999
+		elemToAppend := ""
+		for key, element := range input {
+			if mn < key && mn2 > key {
+				mn2 = key
+				elemToAppend = element
+			}
+		}
+		mn = mn2
+		result = append(result, elemToAppend)
+	}
+
+	return result
 }
